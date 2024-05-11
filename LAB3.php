a9b9b9b72657ae03c7c1ea102d1c5993ee4c914f
Muttakinur <160261658+Muttakinur@users.noreply.github.com>
2024-05-11 18:48:29 +0600

    Add files via upload
    LAB 3 CODE

---
+++ LAB3.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form method="get">
+	ID:<input type="Text" name="id"><br>
+	Password:<input type="Password" name="password"><br>
+	<button name="log">Login</button>
+	<button name="reg">Reg</button>
+	
+</form>
+</body>
+</html>
